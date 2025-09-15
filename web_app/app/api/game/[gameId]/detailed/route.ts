8135fd0a003519874248e08b33006b97f402deca
svickars <sam@thedataface.com>
2025-09-14 21:54:03 -0700

    redeploy

--- web_app/app/api/game/[gameId]/detailed/route.ts
+++ web_app/app/api/game/[gameId]/detailed/route.ts
@@ -43,7 +43,7 @@ export async function GET(request: NextRequest, { params }: { params: { gameId: 
 								{
 									error: 'Failed to parse Python output',
 									output,
-									error,
+									stderr: error,
 								},
 								{ status: 500 }
 							)
